498f293dbb190eef138ec61304d9c409c372a0d7
oxheron <micahogden8@gmail.com>
2023-05-09 12:04:56 -0700

    Can parse old test2.c so it is the only test file now, also added to tests

--- test.c
+++ test.c
@@ -1,8 +1,44 @@
-int test()
+double t = 1;
+
+int spread(int x);
+float test1();
+
+int test() 
 {
-    int a = 4;
-    int* c;
-    int* b = c - 1;
-    long abd = (long) (int*) 12039029032;
-    return abd;
+    int ret = spread(3) + t + test1();
+    return ret + t - 8;
+}
+
+int spread(int x)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        x = x + i / 4;
+    }
+
+    float y = 2.3;
+    int x1 = !y;
+    x1 = -y;
+    int z = x1++ + 3;
+    short three = z;
+
+    return x;
+}
+
+int* blah(int* t)
+{
+    int** test = &t;
+    int y = spread(*t);
+    t = &y;
+    return t;
+}
+
+float test1()
+{
+    float x = 9.823;
+    x = x + 8;
+    x = x + 2.9;
+    int y = 23;
+    t = 9;
+    return x + y;
 }
